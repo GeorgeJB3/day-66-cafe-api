7040588d487ec210abacf513ee571fcb72414a66
George Baldwin <georgebaldwin@Georges-MacBook-Air.local>
2023-11-21 10:43:43 +0000

    created patch_cafe and cafe_not_found functions

--- main.py
+++ main.py
@@ -105,7 +105,21 @@ def post_new_cafe():
 
 
 # HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch_cafe(cafe_id):
+    cafe_to_update = db.get_or_404(Cafe, cafe_id)
+    new_price = request.args.get("new_price")
+    if cafe_to_update:
+        cafe_to_update.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    # else:
+    #     return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
 
+
+@app.errorhandler(404)
+def cafe_not_found(e):
+    return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
 # HTTP DELETE - Delete Record
 
 
